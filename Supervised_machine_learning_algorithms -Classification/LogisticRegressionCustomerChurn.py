f1d848c8e988a71324e51fcb9126bd0166777647
Natasa Vujicic <38683833+natvnu@users.noreply.github.com>
2025-08-08 17:27:56 +0800

    Update and rename LogisticRegressionNew.py to LogisticRegressionCustomerChurn.py

--- Supervised_machine_learning_algorithms -Classification/LogisticRegressionCustomerChurn.py
+++ Supervised_machine_learning_algorithms -Classification/LogisticRegressionCustomerChurn.py
@@ -1,5 +1,7 @@
 #Using a telecommunications dataset for predicting customer churn
 
+#Using a telecommunications dataset for predicting customer churn
+'''
 #1. install libraries
 !pip install pandas
 !pip install scikit-learn
@@ -13,6 +15,9 @@ from sklearn.metrics import log_loss
 import matplotlib.pyplot as plt
 %matplotlib inline 
 
+'''
+
+
 #2. import dataset 
 url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv"
 churn_df = pd.read_csv(url)
@@ -21,10 +26,15 @@ churn_df
 #3. data wrangling
 churn_df.dtypes #target var churn is float, we need to change it to int, it is requirement by the scikit-learn algorithm
 churn_df['churn']=churn_df['churn'].astype('int')
+
+# 4. classifier development
 #define x and y
-x=churn_df.drop(['churn'],axis=1)
+#first round 
+#x=churn_df.drop(['churn'],axis=1) 
+#second round
+x = churn_df[['tenure', 'age', 'address', 'income', 'ed', 'employ',  'callcard','churn']]
 y=churn_df['churn']
-#standardize the dataset, it is a norm
+#scale the dataset
 x_norm = StandardScaler().fit(x).transform(x) #creates np array
 #train-test split the data, 20% for testing
 x_train,x_test,y_train, y_test=train_test_split(x_norm,y,test_size=0.2,random_state=4)
@@ -35,19 +45,17 @@ lr.fit(x_train,y_train)
 #predict class
 yhat=lr.predict(x_test)
 yhat[:10]#array of first 10 predictions
-#predict probability of belonging to the class
+#predict probability of non churn vs churn (0,1)
 yhat_prob = lr.predict_proba(x_test)
 yhat_prob[:10]
-
-
-#Large positive value of LR Coefficient for a given field indicates that increase in this parameter 
-#will lead to better chance of a positive, i.e. 1 class. 
-
-coefficients = pd.Series(lr.coef_[0], index=churn_df.columns[:-1])
-coefficients.sort_values().plot(kind='barh')
-plt.title("Feature Coefficients in Logistic Regression Churn Model")
-plt.xlabel("Coefficient Value")
-plt.show()
-
 #calculate results using log_loss metrics
 print('Log loss is: ', log_loss(y_test, yhat_prob)) #0.7780756315443422, which is not that good, should be closer to 0
+#going back to features and choosing 'tenure', 'age', 'address', 'income', 'ed', 'employ',  'callcard' will reduce log loss to 0.02065304337548963
+
+#to display distribution of predicted probability of customer churn
+plt.hist(y_pred_logr[:, 1], bins=20)  # Show distribution of churn probabilities
+plt.xlabel('Predicted Probability of Churn')
+plt.ylabel('Count')
+plt.show()
+
+
